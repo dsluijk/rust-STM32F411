f8460f853bd965f603d69f3c68de9dc25e844898
Dany Sluijk <dany@atlasdev.nl>
2020-07-27 22:35:00 +0200

    feat: add blink

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,37 @@ use panic_abort as _;
 
 use cortex_m_rt::entry;
 use rtt_target::{rprintln, rtt_init_print};
+use stm32f4xx_hal as hal;
+
+use hal::{prelude::*, stm32};
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!(BlockIfFull);
+
+    if let (Some(dp), Some(cp)) = (
+        stm32::Peripherals::take(),
+        cortex_m::peripheral::Peripherals::take(),
+    ) {
+        // Set up the LED. On the Nucleo-446RE it's connected to pin PA5.
+        let gpiob = dp.GPIOB.split();
+        let mut led = gpiob.pb9.into_push_pull_output();
+
+        // Set up the system clock. We want to run at 48MHz for this one.
+        let rcc = dp.RCC.constrain();
+        let clocks = rcc.cfgr.sysclk(48.mhz()).freeze();
+
+        // Create a delay abstraction based on SysTick
+        let mut delay = hal::delay::Delay::new(cp.SYST, clocks);
+
+        loop {
+            // On for 1s, off for 1s.
+            led.set_high().unwrap();
+            delay.delay_ms(1000_u32);
+            led.set_low().unwrap();
+            delay.delay_ms(1000_u32);
+        }
+    }
 
     let mut i = 0;
     loop {
